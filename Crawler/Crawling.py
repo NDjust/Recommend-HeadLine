a3b0ad07ef9b7698ecaaed3244ee2ff61c9b467d
Nathan <skeks463@gmail.com>
2020-02-01 19:19:23 +0900

    ADd data log code

--- Crawler/Crawling.py
+++ Crawler/Crawling.py
@@ -51,14 +51,20 @@ def get_data(url):
 
     for title_tag in soup.select('dt > a > span'):
         titles.append(title_tag.text)
+        print(f"title: \n{title_tag.text}\n")
 
     for view_tag in soup.select('div > em'):
         views.append(view_tag.text)
+        print(f"views: \n{view_tag.text}\n")
 
     for link_tag in soup.select('div > dt > a'):
         link = "https:" + link_tag["href"]
         article_link.append(link)
-        contents.append(get_content(link))
+        content = get_content(link)
+        contents.append(content)
+
+        print(f"title: \n{link}\n")
+        print(f"title: \n{content}\n")
 
     return titles, views, article_link, contents
 
