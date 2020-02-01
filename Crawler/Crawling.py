06bdc18c087d9630a7f21382bb49e1e59e9ef830
Nathan <skeks463@gmail.com>
2020-02-01 19:07:51 +0900

    Refactoring (Split function)

--- Crawler/Crawling.py
+++ Crawler/Crawling.py
@@ -1,19 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchAttributeException, InvalidSelectorException
 from bs4 import BeautifulSoup
 
-import datetime
-import requests
-
 PATH = "./webdriver/chromedriver"
-URL = "http://news.chosun.com/ranking/list.html?type=&site=www&scode=star&term=&date="
 
 
 def load_chrome_browser():
-    """
-    Load chrome brawser function. (to remove overlap code)
+    """ Load chrome browser function.
+
     :return: Chrome browser Object.
     """
     chrome_options = Options()
@@ -25,48 +19,46 @@ def load_chrome_browser():
 
 
 def get_content(link):
-    pass
+    """ Get Article content.
+
+        :param link: Article Url.
+        :return: Article content.
+        """
+    driver = load_chrome_browser()
+    driver.get(link)
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, "html.parser")
+    body = ""
+
+    for content in soup.find_all("div", {"class": "par"}):
+        body += content.text
+
+    return body
 
 
-def get_data(start, end, url):
-    date_range = []
-    date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days)]
+def get_data(url):
     driver = load_chrome_browser()
 
-    for date in date_generated:
-        date_range.append(date.strftime("%Y%m%d"))
+    titles = []
+    views = []
+    article_link = []
+    contents = []
 
-    title = []
-    heart = []
-    link_ = []
-    content = []
+    driver.get(url)
+    a = driver.page_source
 
-    for i in date_range:
-        driver.get(url + i)
-        a = driver.page_source
+    soup = BeautifulSoup(a, 'html.parser')
 
-        soup = BeautifulSoup(a, 'html.parser')
+    for title_tag in soup.select('dt > a > span'):
+        titles.append(title_tag.text)
 
-        for tag in soup.select('dt > a > span'):
-            title.append(tag.text)
+    for view_tag in soup.select('div > em'):
+        views.append(view_tag.text)
 
-        for names in soup.select('div > em'):
-            heart.append(names.text)
+    for link_tag in soup.select('div > dt > a'):
+        link = "https:" + link_tag["href"]
+        article_link.append(link)
+        contents.append(get_content(link))
 
-        for tags in soup.select('div > dt > a'):
-            link_.append(tags['href'])
+    return titles, views, article_link, contents
 
-        # Top 30에 있는 본문 링크를 가져와 링크타고 본문 스크래핑.
-        link2_ = '[]'.join(link_)
-        link2_ = link2_.replace('https:', '')
-        link3_ = link2_.split('[]')
-
-        for i in link3_:
-            base = requests.get('https:' + i)
-            base.encoding = 'utf-8'
-            soup = BeautifulSoup(base.text, 'html.parser', from_encoding='utf-8')
-
-            for contents in soup.find_all('div', class_='par'):
-                content.append(contents.text)
-
-    return title, heart, link_, content
