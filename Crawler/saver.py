6ef9f258cb44d56552bd984241a938df3d049ef2
Nathan <skeks463@gmail.com>
2020-02-01 19:10:47 +0900

    Add Data Saver

---
+++ Crawler/saver.py
@@ -0,0 +1,49 @@
+from pymysql import Error
+
+import pymysql
+import os
+import csv
+import pandas as pd
+
+
+def save_db(table_name, title, views, article_link, content):
+    try:
+        conn = pymysql.connect(host="", user="",
+                               password="", db="",
+                               charset="utf8")
+        print(conn.get_server_info())
+
+        cursor = conn.cursor()
+
+        print("\nSave data in DB")
+
+        for i in range(len(title)):
+            sql = f"insert into {table_name} values(" \
+                  f"{title[i]}, {views[i]}, {article_link[i]}, {content[i]})"
+
+        cursor.execute(sql)
+        conn.commit()
+
+        conn.close()
+
+    except Error as e:
+        print(e)
+
+
+def save_csv(title, views, article_link, content, file_name):
+    data = []
+    for i in range(len(title)):
+        data.append([title[i], views[i], article_link[i], content[i]])
+
+    if file_name not in os.listdir("./data/"):
+        print("=========Generate CSV File==============")
+        df = pd.DataFrame(data, columns=["title", "views", "article_link", "content"])
+        df.to_csv(f'./data/{file_name}.csv', encoding="utf-8-sig", index=False)
+    else:
+        with open(f"./data/{file_name}.csv", "w") as f:
+            print("========Add data in CSV File===========")
+            writer = csv.writer(f)
+            writer.writerows(data)
+
+    return
+
