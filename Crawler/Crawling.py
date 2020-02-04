87c54f14b664c4bd07a083db2753a4cd5ce28e64
Nathan <skeks463@gmail.com>
2020-02-04 21:59:37 +0900

    Improve speed webdriver

--- Crawler/Crawling.py
+++ Crawler/Crawling.py
@@ -1,11 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import WebDriverException, TimeoutException
 from bs4 import BeautifulSoup
 from saver import *
-
-import requests
-from datetime import datetime
+from selenium import webdriver
 
 PATH = "./webdriver/chromedriver"
 
@@ -19,9 +15,23 @@ def set_chrome_browser():
     options.add_argument("headless")
     options.add_argument("window-size=1920x1080")
     options.add_argument("disable-gpu")
+    options.add_argument('headless')  # headless 모드 설정
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
 
-    browser = webdriver.Chrome(
-            "./webdriver/chromedriver", options=options)
+    # 속도 향상을 위한 옵션 해제
+    prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'plugins': 2, 'popups': 2,
+                                                        'geolocation': 2, 'notifications': 2,
+                                                        'auto_select_certificate': 2, 'fullscreen': 2, 'mouselock': 2,
+                                                        'mixed_script': 2, 'media_stream': 2, 'media_stream_mic': 2,
+                                                        'media_stream_camera': 2, 'protocol_handlers': 2,
+                                                        'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2,
+                                                        'push_messaging': 2, 'ssl_cert_decisions': 2,
+                                                        'metro_switch_to_desktop': 2, 'protected_media_identifier': 2,
+                                                        'app_banner': 2, 'site_engagement': 2, 'durable_storage': 2}}
+    options.add_experimental_option('prefs', prefs)
+
+    browser = webdriver.Chrome(PATH, options=options)
 
     return browser
 
