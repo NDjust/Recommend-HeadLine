fef675b3009ff53d8dc5848beba7933412bfa669
Nathan <skeks463@gmail.com>
2020-02-12 13:59:19 +0900

    Renew Multiprocessing method

--- TextPreprocessing/preprocessor.py
+++ TextPreprocessing/preprocessor.py
@@ -58,7 +58,7 @@ class TextPreProcessor(object):
         return words
 
     @staticmethod
-    def review_to_pos_words(text: list, remove_stopwords=False) -> list:
+    def review_to_pos_words(text: str, remove_stopwords=False) -> list:
         """
 
         :param text: 텍스트 데이터
@@ -88,7 +88,7 @@ class TextPreProcessor(object):
         return result
 
     @staticmethod
-    def review_to_join_words(text: list, remove_stopwords=False) -> str:
+    def review_to_join_words(text: str, remove_stopwords=False) -> str:
         """ word 단위로 토큰화 된 text 데이터를 합쳐주는 함수.
 
         :param text: input review or text data.
@@ -99,9 +99,8 @@ class TextPreProcessor(object):
         join_words = ' '.join(words)
         return join_words
 
-
     @staticmethod
-    def review_to_sentences(text: list, remove_stopwords=False) -> list:
+    def review_to_sentences(text: str, remove_stopwords=False) -> list:
         """ 불용어 및 전처리 된 문장 데이터를 만들어 주는 함수.
 
         :param text: input review or text data.
@@ -124,7 +123,7 @@ class TextPreProcessor(object):
         return sentences
 
     @staticmethod
-    def remove_emoji(text):
+    def remove_emoji(text: str):
         """ Remove emoji data.
 
         :param df: dataframe
@@ -133,23 +132,14 @@ class TextPreProcessor(object):
         EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
         return EMOJI.sub(r'', text)
 
-    # 참고 : https://gist.github.com/yong27/7869662
-    # http://www.racketracer.com/2016/07/06/pandas-in-parallel/
-    # 속도 개선을 위해 멀티 스레드로 작업하도록
     @staticmethod
-    def _apply_df(args):
-        df, func, kwargs = args
-        return df.apply(func, **kwargs)
-
-    @staticmethod
-    def apply_by_multiprocessing(df: pd.DataFrame, func, **kwargs) -> pd.DataFrame:
+    def apply_by_multiprocessing(data: list, func, **kwargs) -> list:
         # 키워드 항목 중 workers 파라메터를 꺼냄
         workers = kwargs.pop('workers')
         # 위에서 가져온 workers 수로 프로세스 풀을 정의
         pool = Pool(processes=workers)
         # 실행할 함수와 데이터프레임을 워커의 수 만큼 나눠 작업
-        result = pool.map(TextPreProcessor._apply_df, [(d, func, kwargs)
-                for d in np.array_split(df, workers)])
+        result = pool.map(func, data)
         pool.close()
         # 작업 결과를 합쳐서 반환
-        return pd.concat(result)
+        return result
