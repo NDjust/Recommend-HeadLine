1b326d17446929b1597831d634c8297ef55a1da1
Nathan <skeks463@gmail.com>
2020-02-19 17:14:25 +0900

    Refacotring & Add make dictionary method

--- Model/transfer.py
+++ Model/transfer.py
@@ -1,51 +1,82 @@
 from TextPreprocessing.preprocessor import TextPreProcessor
-from load import get_data
 from gensim.models import Word2Vec
+from TextPreprocessing.MysqlHandler import MysqlHandler
+from utils import get_data
 
-trained_model = Word2Vec.load("./trainedModel/ko/ko.bin")
+
+HOST = ""
+USER = ""
+PASSWORD = ""
+PORT = 0
+
+DB_TABLE_1 = ""
+DB_TABLE_2 = ""
+
+date = []
+data_handler = MysqlHandler(host=HOST, user=USER,
+                            password=PASSWORD, port=PORT)
+
+trained_model = Word2Vec.load("path")
 
 
-def train_data():
-    data = get_data("title")
+def train_data(save=False, path=None):
 
-    print("Title data PreProcessing")
-    data = TextPreProcessor.apply_by_multiprocessing(
-        func=TextPreProcessor.text_to_wordlist, data=data,
-        workers=4, stopwords=True, tokenizer="okt"
-    )
+    for start, end in date:
+        print(f"Start {start} date")
+        field = "title"
+        sql = f"select {field} from {DB_TABLE_1} " \
+              f"where created_date >= {start} and created_date < {end}"
 
-    print("Word2Vec Build vocab by using Title data ")
-    result = trained_model.build_vocab(data, update=True)
+        data = get_data(sql=sql)
 
-    print("Word2Vec train by using Title data ")
-    result = trained_model.train(data,
-                                 total_examples=trained_model.corpus_count,
-                                 epochs=trained_model.epochs)
-    print("Complete title data train")
-    print(result)
-    print(trained_model)
+        print("Title data PreProcessing")
+        data = TextPreProcessor.apply_by_multiprocessing(
+            func=TextPreProcessor.text_to_wordlist, data=data,
+            workers=4, stopwords=True, tokenizer="okt"
+        )
+        print(data)
 
-    print("content data PreProcessing")
-    data = get_data("content")
+        print("Word2Vec Build vocab by using Title data ")
+        result = trained_model.build_vocab(data, update=True)
 
-    data = TextPreProcessor.apply_by_multiprocessing(
-        func=TextPreProcessor.text_to_wordlist, data=data,
-        workers=4, stopwords=True, tokenizer="okt"
-    )
-    print("Word2Vec Build vocab by using content data ")
-    result = trained_model.build_vocab(data, update=True)
+        print("Word2Vec train by using Title data ")
+        result = trained_model.train(data,
+                                     total_examples=trained_model.corpus_count,
+                                     epochs=trained_model.epochs)
+        print("Complete title data train")
+        print(result)
+        print(trained_model)
 
-    print("Word2Vec train by using Title data ")
-    result = trained_model.train(data,
-                                 total_examples=trained_model.corpus_count,
-                                 epochs=trained_model.epochs)
+        field = "content"
+        sql = f"select {field} from {DB_TABLE_1} " \
+              f"where created_date >= {start} and created_date < {end}"
 
-    print("Complete title data train")
-    print(result)
-    print(trained_model)
+        data = get_data(sql=sql)
+
+        print("content data PreProcessing")
+        data = TextPreProcessor.apply_by_multiprocessing(
+            func=TextPreProcessor.text_to_wordlist, data=data,
+            workers=4, stopwords=True, tokenizer="okt"
+        )
+
+        print("Word2Vec Build vocab by using content data ")
+        result = trained_model.build_vocab(data, update=True)
+
+        print("Word2Vec train by using content data ")
+        result = trained_model.train(data,
+                                     total_examples=trained_model.corpus_count,
+                                     epochs=trained_model.epochs)
+
+        print("Complete content data train")
+        print(result)
+        print(trained_model)
+
+    if save:
+        trained_model.save(path)
+
     return trained_model
 
 
 if __name__ == "__main__":
-    model = train_data()
-    model.save("./trainedModel/transferW2V.model")
+    path = ""
+    model = train_data(save=True, path=path)
