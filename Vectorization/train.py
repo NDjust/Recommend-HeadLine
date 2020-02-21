f0efcab8cca9d19dc2d29515c5faef254c8de774
Nathan <skeks463@gmail.com>
2020-02-21 11:39:16 +0900

    Split Responsibility

--- Vectorization/train.py
+++ Vectorization/train.py
@@ -1,86 +1,60 @@
-from TextPreprocessing.preprocessor import TextPreProcessor
 from gensim.models import Word2Vec
 from TextPreprocessing.MysqlHandler import MysqlHandler
-from utils import get_data
+from utils import get_data, get_corpus
+from Vectorizer import train_word2vec, transfer_word2vec
+from Vectorizer import load_model, train_n_gram, train_tfidf
 
 
-HOST = ""
-USER = ""
-PASSWORD = ""
-PORT = 0
-
-DB_TABLE_1 = ""
-DB_TABLE_2 = ""
-
-date = []
-data_handler = MysqlHandler(host=HOST, user=USER,
-                            password=PASSWORD, port=PORT)
-
-
-def load_model(path):
-    model = Word2Vec.load(path)
-
-    return model
-
-
-def get_corpus(data) -> list:
-    print("Convert data to corpus data")
-
-    data = TextPreProcessor.apply_by_multiprocessing(
-        func=TextPreProcessor.text_to_wordlist, data=data,
-        workers=4, stopwords=True, tokenizer="okt"
-    )
-
-    return data
-
-
-def train_word2vec(data):
-    model = Word2Vec(sentences=data,
-                     size=100, window=5,
-                     min_count=1, workers=4)
-
-    return model
-
-
-def transfer_word2vec(trained_model, corpus):
-    # Update Vocab
-    trained_model.build_vocab(corpus, update=True)
-
-    print("Word2Vec train by using Title data ")
-    result = trained_model.train(corpus,
-                                 total_examples=trained_model.corpus_count,
-                                 epochs=trained_model.epochs)
-    print("Complete train")
-    return result
-
-
-def train_data(sql, transfer=False, save=False, path=None):
-    """ Word2Vec transfer learning by using collected data.
+def train(data, method="word2vec", n=None, transfer=False, save=False, path=None):
+    """ Train Vectorizing Model by using collected data.
 
     Trained Vectorization Reference - https://github.com/Kyubyong/wordvectors
+    :param data: input data for train.
+    :param method: Vectorizer Method (word2vec, TF-IDF, n-gram)
+    :param n: select counts for n-gram model.
+    :param transfer: transfer learning existence.
     :param save: save existence.
     :param path: save path.
     :return: transfer trained model.
     """
-
-    data = get_data(sql)
     total_corpus = []
 
     for d in data:
         total_corpus += get_corpus(d)
 
-    if transfer:
-        trained_model = load_model(path)
-        trained_model = transfer_word2vec(trained_model, total_corpus)
+    if method == "tfidf":
+        trained_model = train_tfidf(data)
+    elif method == "ngram":
+        if n is not None:
+            trained_model = train_n_gram(data, n)
+        else:
+            trained_model = train_n_gram(data, 2)
     else:
-        trained_model = train_word2vec(total_corpus)
+        if transfer:
+            trained_model = load_model(path)
+            trained_model = transfer_word2vec(trained_model, total_corpus)
+        else:
+            trained_model = train_word2vec(total_corpus)
 
     if save:
-        trained_model.save(path)
+        if path is not None:
+            trained_model.save(path)
+        else:
+            trained_model.save("Vectorizer.model")
 
     return trained_model
 
 
-
 if __name__ == "__main__":
-    pass
+    HOST = ""
+    USER = ""
+    PASSWORD = ""
+    PORT = 0
+
+    handler = MysqlHandler(host=HOST, user=USER,
+                           password=PASSWORD, port=PORT)
+
+    sql = ""
+    data = get_data(sql, handler)
+    method = ""
+    train(data, method=method)
